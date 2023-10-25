9051262da79d455883c7eda76cd83042318651a1
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-10-25 19:35:56 +0200

    update

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
-    libTyphon = typhon.actions.${system};
+    # libTyphon = typhon.actions.${system};
     mk = name: duration: fails: let
       value = pkgs.stdenv.mkDerivation {
         inherit name;
@@ -31,28 +31,19 @@
       };
     in {inherit name value;};
   in {
-    typhonProject = libTyphon.mkProject {
-      meta = {
-        title = "Test";
-        description = "testing Typhon";
-        homepage = "https://typhon-ci.org";
-      };
-      actions = {
-        jobsets = libTyphon.mkGitJobsets {url = "/home/lucas/repos/typhon-test";};
-      };
+    typhonProject = typhon.lib.github.mkGithubProject {
+      owner = "W95Psp";
+      repo = "typhon-test";
+      secrets = null;
+      typhon_url = "dummy";
     };
-    typhonJobs =
-      (pkgs.lib.listToAttrs [
-        (mk "rustfmt" 0.5 false)
-        (mk "check english" 0.3 false)
-        (mk "tests" 2 false)
-        (mk "build" 1.5 false)
-        (mk "more tests" 4 95)
-        (mk "enforce 80 width" 0.1 false)
-      ])
-      // {
-        # dev = make {name = "dev"; duration = 0.5;};
-        # feature-foo = make {name = "feature-one"; duration = 0.5;};
-      };
+    typhonJobs."${system}" = pkgs.lib.listToAttrs [
+      (mk "rustfmt" 0.5 false)
+      (mk "check english" 0.3 false)
+      (mk "tests" 2 false)
+      (mk "build" 1.5 false)
+      (mk "more tests" 4 95)
+      (mk "enforce 80 width" 0.1 false)
+    ];
   };
 }
