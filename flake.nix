dc77d46a8d59b0cc049cc001b9a71bd0bb050cb9
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-10-25 19:19:38 +0200

    update

--- flake.nix
+++ flake.nix
@@ -4,39 +4,55 @@
     typhon.url = "github:typhon-ci/typhon";
   };
 
-  outputs = { self, nixpkgs, typhon }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      libTyphon = typhon.actions.${system};
-    in {
-      typhonProject = libTyphon.mkProject {
-        meta = {
-          title = "Test";
-          description = "testing Typhon";
-          homepage = "https://typhon-ci.org";
-        };
-        actions = {
-          jobsets = libTyphon.gitJobsets "https://github.com/typhon-ci/test";
-        };
-      };
-      typhonJobs = {
-        hello = pkgs.stdenv.mkDerivation {
-          name = "hello";
-          src = ./hello;
-          configurePhase = ''
-            echo Hello
-            for i in $(seq 0 1 100); do
-              printf "% 4d%s\n" $i '%'
-              sleep 1
+  outputs = {
+    self,
+    nixpkgs,
+    typhon,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    libTyphon = typhon.actions.${system};
+    mk = name: duration: fails: let
+      value = pkgs.stdenv.mkDerivation {
+        inherit name;
+        src = ./hello;
+        configurePhase = ''
+          echo '${name}'
+          for i in $(seq 0 1 100); do
+            printf "% 4d%s\n" $i '%'
+            if [[ "$i" -eq '${fails}' ]];
+              echo "failure" 1>&2
+              exit 1
             done
-            exit 1
-            export PREFIX=$out
-          '';
-        };
-        # failing = pkgs.stdenv.mkDerivation {
-        #   name = "failing";
-        # };
+            sleep '${toString duration}'
+          done
+          export PREFIX=$out
+        '';
+      };
+    in {inherit name value;};
+  in {
+    typhonProject = libTyphon.mkProject {
+      meta = {
+        title = "Test";
+        description = "testing Typhon";
+        homepage = "https://typhon-ci.org";
+      };
+      actions = {
+        jobsets = libTyphon.mkGitJobsets {url = "/home/lucas/repos/typhon-test";};
       };
     };
+    typhonJobs =
+      (pkgs.lib.listToAttrs [
+        (mk "rustfmt" 0.5 false)
+        (mk "check english" 0.3 false)
+        (mk "tests" 2 false)
+        (mk "build" 1.5 false)
+        (mk "more tests" 4 95)
+        (mk "enforce 80 width" 0.1 false)
+      ])
+      // {
+        # dev = make {name = "dev"; duration = 0.5;};
+        # feature-foo = make {name = "feature-one"; duration = 0.5;};
+      };
+  };
 }
