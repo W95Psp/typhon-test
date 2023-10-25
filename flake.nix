3dd02e250c11555750d49c405ecde69aeb044a2c
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-10-25 20:17:25 +0200

    more

--- flake.nix
+++ flake.nix
@@ -12,23 +12,33 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
     # libTyphon = typhon.actions.${system};
-    mk = name: duration: fails: let
+    mk = name: duration: {
+      fails ? false,
+      infinite ? false,
+    }: let
       value = pkgs.stdenv.mkDerivation {
         inherit name;
         src = ./hello;
         configurePhase = ''
           echo '${name}'
-          for i in $(seq 0 1 100); do
-            printf "% 4d%s\n" $i '%'
-            if [[ "$i" -eq "${
+          while true; do
+            for i in $(seq 0 1 100); do
+              printf "% 4d%s\n" $i '%'
+              if [[ "$i" -eq "${
             if fails == false
             then "101"
             else toString fails
           }" ]]; then
-              echo "failure" 1>&2
-              exit 1
-            fi
-            sleep '${toString duration}'
+                echo "failure" 1>&2
+                exit 1
+              fi
+              sleep '${toString duration}'
+            done
+            echo "${
+            if infinite
+            then "repeting"
+            else "compiling now"
+          }"
           done
           export PREFIX=$out
         '';
@@ -42,12 +52,13 @@
       typhon_url = "dummy";
     };
     typhonJobs."${system}" = pkgs.lib.listToAttrs [
-      (mk "rustfmt" 0.5 false)
-      (mk "check english" 0.3 false)
-      (mk "tests" 2 false)
-      (mk "build" 1.5 false)
-      (mk "more tests" 4 95)
-      (mk "enforce 80 width" 0.1 false)
+      (mk "rustfmt" 0.5 {})
+      (mk "check english" 0.3 {})
+      (mk "tests" 2 {infinite = true;})
+      (mk "build (debug)" 2.5 {infinite = true;})
+      (mk "build" 1.5 {infinite = true;})
+      (mk "more tests" 4 {fails = 95;})
+      (mk "enforce 80 width" 0.1 {})
     ];
   };
 }
